67e2eb2a6e7a40395d744380a524123ec8aa8d2a
MatheusCanuto07 <matheushenriquecanuto77@gmail.com>
2024-12-15 19:59:24 -0300

    tela de registrar Material

--- front/front-stock/src/routes/registerMaterial/+page.svelte
+++ front/front-stock/src/routes/registerMaterial/+page.svelte
@@ -1,7 +1,41 @@
 <script lang="ts">
     import type { PageData } from './$types';
+    let CNPJ = $state("11111111111111");
+    let CEP = $state("11111111");
+    let Date = $state("1972-02-03");
+    let QRCODE = $state("");
 
     let { data }: { data: PageData } = $props();
 </script>
 
-<h2>registerMaterial</h2>
+<h2 class="text-center mb-5 font-medium text-2xl">Register Material</h2>
+
+<div class="w-full flex">
+  <div class="w-1/2">
+    <p>Selecione o fornecedor</p>
+    <select class="select w-full max-w-xs">
+      <option disabled selected>Pick your favorite Simpson</option>
+      <option>Homer</option>
+      <option>Marge</option>
+      <option>Bart</option>
+      <option>Lisa</option>
+      <option>Maggie</option>
+    </select>
+  </div>
+  <div class="w-1/2">
+    <p>QRCODE</p>
+    {QRCODE !== "" ? QRCODE : ""}
+  </div>
+</div>
+
+<div class="w-full flex mt-3">
+  <div class="w-1/2">
+    <p>Select the registration date</p>
+    <input bind:value={Date} type="date" class="input w-full max-w-xs" required/>
+  </div>
+  <div class="w-1/2 flex items-end">
+    <button class="btn w-full btn-success" >
+      Enviar
+    </button>
+  </div>
+</div>
--- front/front-stock/src/routes/registerSupplier/+page.svelte
+++ front/front-stock/src/routes/registerSupplier/+page.svelte
@@ -1,7 +1,44 @@
 <script lang="ts">
     import type { PageData } from './$types';
+    let CNPJ = $state("11111111111111");
+    let CEP = $state("11111111");
+    let Date = $state("1972-02-03");
+
+    $inspect(CNPJ, CEP, Date);
+
+    function submit(){
+      if(CNPJ !== "" && CNPJ.length == 14 && CEP !== "" && CEP.length == 8 && Date !== ""){
+        let string = `${CNPJ}%${CEP}%${Date}`;
+        console.log(string)
+      } else{
+        alert("Dados inválidos");
+      }
+    }
 
     let { data }: { data: PageData } = $props();
 </script>
 
-<h2>registerSupplier</h2>
+<h1 class="text-center mb-5 font-medium text-2xl">Register Supplier</h1>
+
+<div class="w-full flex">
+  <div class="w-1/2">
+    <p>Enter your CNPJ:</p>
+    <input bind:value={CNPJ} type="text" placeholder="Type here" class="input w-full max-w-xs" required/>
+  </div>
+  <div class="w-1/2">
+    <p>Enter your CEP:</p>
+    <input bind:value={CEP} type="text" placeholder="Type here" class="input w-full max-w-xs" required/>
+  </div>
+</div>
+
+<div class="w-full flex mt-3">
+  <div class="w-1/2">
+    <p>Select the registration date</p>
+    <input bind:value={Date} type="date" class="input w-full max-w-xs" required/>
+  </div>
+  <div class="w-1/2 flex items-end">
+    <button class="btn w-full btn-success" onclick={submit}>
+      Enviar
+    </button>
+  </div>
+</div>
