e3b7c6aeaa59fd638bc870bdbdd7e382c7c4faeb
MatheusCanuto07 <matheushenriquecanuto77@gmail.com>
2024-12-15 21:03:04 -0300

    Feature QRcode

--- front/front-stock/src/routes/registerMaterial/+page.svelte
+++ front/front-stock/src/routes/registerMaterial/+page.svelte
@@ -1,11 +1,30 @@
 <script lang="ts">
-    import type { PageData } from './$types';
-    let CNPJ = $state("11111111111111");
-    let CEP = $state("11111111");
-    let Date = $state("1972-02-03");
-    let QRCODE = $state("");
+  import { onMount } from 'svelte';
+  let QRCODE = $state("");
+  let supplier = $state([]);
+  let selectHTML : HTMLSelectElement | null = $state(null);
+  let supplierSelected = "";
+  
+  onMount(async () => {
+    fetch("http://localhost:5266/api/v1/supplier")
+    .then(response => response.json())
+    .then(dados => {
+      supplier = dados;
+      console.log(dados);
+    }).catch(error => {
+      console.log(error);
+      return [];
+    });
+  })
+  
+  function loadQRCODE(selectId){
+    console.log(selectId)
+    let aux = $state.snapshot(supplier).find(element => element.id === selectId);
+    QRCODE = aux.qrCode.replace(/%([^%]+)%/g, '$1');
+    //supplierSelected = supplier.qrCode.replace(/%([^%]+)%/g, '$1');
+  } 
 
-    let { data }: { data: PageData } = $props();
+  let { data }: { data: ImageData } = $props();
 </script>
 
 <h2 class="text-center mb-5 font-medium text-2xl">Register Material</h2>
@@ -13,13 +32,11 @@
 <div class="w-full flex">
   <div class="w-1/2">
     <p>Selecione o fornecedor</p>
-    <select class="select w-full max-w-xs">
-      <option disabled selected>Pick your favorite Simpson</option>
-      <option>Homer</option>
-      <option>Marge</option>
-      <option>Bart</option>
-      <option>Lisa</option>
-      <option>Maggie</option>
+    <select bind:value={selectHTML} onchange={() => loadQRCODE(selectHTML)} class="select w-full max-w-xs">
+      <option disabled selected>Selecione um fornecedor</option>
+      {#each supplier as s}
+      <option value={s.id}>{s.name}</option>
+      {/each}
     </select>
   </div>
   <div class="w-1/2">
@@ -31,7 +48,7 @@
 <div class="w-full flex mt-3">
   <div class="w-1/2">
     <p>Select the registration date</p>
-    <input bind:value={Date} type="date" class="input w-full max-w-xs" required/>
+    <input type="date" class="input w-full max-w-xs" required/>
   </div>
   <div class="w-1/2 flex items-end">
     <button class="btn w-full btn-success" >
--- front/front-stock/src/routes/registerSupplier/+page.svelte
+++ front/front-stock/src/routes/registerSupplier/+page.svelte
@@ -1,21 +1,47 @@
 <script lang="ts">
-    import type { PageData } from './$types';
-    let CNPJ = $state("11111111111111");
-    let CEP = $state("11111111");
-    let Date = $state("1972-02-03");
+  import type { PageData } from './$types';
+  let CNPJ = $state("11111111111111");
+  let name = $state("11111111111111");
+  let CEP = $state("11111111");
+  let Date = $state("1972-02-03");
+  
+  $inspect(CNPJ, CEP, Date);
+  
+  function submit(){
+    if(CNPJ !== "" && CNPJ.length == 14 && CEP !== "" && CEP.length == 8 && Date !== "" && name !== ""){
+      let qrCode = `%${CNPJ}% - %${CEP}% / Cad.%${Date}%`;
+      console.log(qrCode)
 
-    $inspect(CNPJ, CEP, Date);
-
-    function submit(){
-      if(CNPJ !== "" && CNPJ.length == 14 && CEP !== "" && CEP.length == 8 && Date !== ""){
-        let string = `${CNPJ}%${CEP}%${Date}`;
-        console.log(string)
-      } else{
-        alert("Dados inválidos");
+      let dataSend = {
+        address: "Rua Exemplo, 123",
+        QRCode: qrCode,
+        cnpj: CNPJ,
+        cep: CEP,
+        name: name
       }
-    }
 
-    let { data }: { data: PageData } = $props();
+      console.log(JSON.stringify(dataSend));
+
+      fetch("http://localhost:5266/api/v1/supplier", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json" 
+        },
+        body: JSON.stringify(dataSend) 
+      })
+      .then(response => response.json()) 
+      .then(data => {
+        console.log("Resposta do servidor:", data); 
+      })
+      .catch(error => {
+        console.error("Erro na requisição:", error); 
+      });
+    } else{
+      alert("Dados inválidos");
+    }
+  }
+  
+  let { data }: { data: PageData } = $props();
 </script>
 
 <h1 class="text-center mb-5 font-medium text-2xl">Register Supplier</h1>
@@ -29,6 +55,11 @@
     <p>Enter your CEP:</p>
     <input bind:value={CEP} type="text" placeholder="Type here" class="input w-full max-w-xs" required/>
   </div>
+</div>
+
+<div class="w-full">
+  <p>Enter your name:</p>
+  <input bind:value={name} type="text" placeholder="Type here" class="input w-full" required/>
 </div>
 
 <div class="w-full flex mt-3">
